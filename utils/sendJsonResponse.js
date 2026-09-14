ec4da4188725a00215b9780a4bec9047ac4b474f
luizfelipe5g <luiz.felipe.s@protonmail.com>
2026-09-14 20:37:53 -0300

    feat: adiciona CORS para permitir requisições de qualquer frontend

--- utils/sendJsonResponse.js
+++ utils/sendJsonResponse.js
@@ -1,5 +1,7 @@
 export const sendJsonResponse = (response, statusCode, payload) => {
     response.setHeader("Content-Type", "application/json");
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Request-Method", "GET");
     response.statusCode = statusCode;
     response.end(JSON.stringify(payload));
 }
